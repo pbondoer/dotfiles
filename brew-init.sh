43b3bdfeaec86da65c445b05520ebbd3a333a50e
Pierre BONDOERFFER <pbondoer@student.42.fr>
2016-12-20 14:28:46 +0100

    brew init script 🍺

---
+++ brew-init.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+source color.sh
+
+log $CYAN "Performing first time setup"
+
+log $GREEN "Removing old install..."
+rm -rf $HOME/.brew
+
+log $GREEN "Installing..."
+/usr/local/bin/brew update
+
+log $GREEN "Updating..."
+$HOME/.brew/bin/brew update
+
+echo 'export PATH=$HOME/.brew/bin:$PATH' >> $HOME/.zshrc
+log $YELLOW "Done! Brew installed, added to PATH and exported in .zshrc"
+printf "To install packages, use "
+tput setaf $RED
+printf "brew install package"
+log $YELLOW "."
