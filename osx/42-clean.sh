a8b81b9995530acf57b6c6e011114d90788f5290
Pierre BONDOERFFER <pbondoer@student.42.fr>
2017-12-06 04:12:09 +0100

    42: better clean script

--- osx/42-clean.sh
+++ osx/42-clean.sh
@@ -4,14 +4,27 @@ source color.sh
 TITLE=$MAGENTA
 STEP=$CYAN
 SUCCESS=$GREEN
+DANGER=$RED
 SIZE=$YELLOW
-
-total=0
 
 log() {
 	tput setaf $1
 	printf "$2"
 }
+
+filelist=()
+total=0
+
+# bash options, to be undone at exit
+shopt -u | grep -q nullglob && change_null=true && shopt -s nullglob
+shopt -u | grep -q dotglob && change_dot=true && shopt -s dotglob
+
+undoopts() {
+	[ $change_null ] && shopt -u nullglob; unset change_null
+	[ $change_dot ] && shopt -u dotglob; unset change_dot
+}
+
+trap undoopts INT TERM EXIT
 
 # https://unix.stackexchange.com/a/259254
 bytefmt() {
@@ -25,28 +38,62 @@ bytefmt() {
 	echo "$b$d ${S[$s]}"
 }
 
+# Items
 item() {
-	size=`du -sck ${@:1} 2>/dev/null | tail -n1 | cut -d $'\t' -f1 | tr -d ' '`
+	size=`du -I . -sck ${@:1} 2>/dev/null | tail -n1 | cut -d $'\t' -f1 | tr -d ' '`
 	total=$((total + size))
-	rm -rf ${@:1} 2>/dev/null
+	filelist=("${filelist[@]}" "${@:1}")
 	log $SIZE "$(bytefmt $size)\n"
+	for file in ${@:1}
+	do
+		log $WHITE " -> "
+		log $SIZE "$file\n"
+	done
 }
 
-log $TITLE "Cleaning up junk files...\n\n"
+log $TITLE "Finding junk files...\n\n"
 
 log $STEP "42 Library backups ... "
-item "$HOME/Library/*.42_cache*" "$HOME/*.42_cache*"
+item $HOME/Library/*.42_cache_bak_* $HOME/*.42_cache_bak_*
 
 log $STEP "NPM cache ... "
-item "$HOME/.npm"
+item $HOME/.npm/*
 
 log $STEP "OS X Trash ... "
-item "$HOME/.Trash/*"
+item $HOME/.Trash/*
 
 log $STEP "~/.cache ... "
-item "$HOME/.cache"
+item $HOME/.cache/*
 
 log $STEP "Brew ... "
-item `brew cleanup -n 2> /dev/null | cut -d ' ' -f3 | tr -d ' ' | grep -v operation` `brew --cache`
+item `brew cleanup -n 2> /dev/null | cut -d ' ' -f3 | tr -d ' ' | grep -v operation` `brew --cache`/*
 
-log $SUCCESS "\nDone! Removed $(bytefmt $total)\n"
+log $WHITE "\n"
+
+if [[ -z "${filelist[@]}" ]]
+then
+	log $SUCCESS "Done! No files to delete.\n"
+	exit 0
+fi
+
+log $DANGER "/!\\ WARNING - DANGER ZONE /!\\ \n"
+log $DANGER "This will erase ${#filelist[@]} files listed above.\n"
+
+log $WHITE "\n"
+read -p "Continue (y/n)? " -n 1 -r
+echo "\n" # add some padding
+
+if ! [[ $REPLY =~ ^[Yy]$ ]]
+then
+	log $STEP "Aborted by user.\n"
+	exit 0
+fi
+
+log $STEP "Removing...\n"
+rm -rf ${filelist[@]}
+
+log $SUCCESS "\nDone! Removed "
+log $SIZE "${#filelist[@]} files"
+log $SUCCESS " and freed "
+log $SIZE "$(bytefmt $total)"
+log $SUCCESS ".\n"
